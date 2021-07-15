be387c24b327ba501fbdbbd474ccdf69256f2546
robertoAureli <r.aureli@protonmail.com>
2021-07-15 14:38:23 +0200

    Fix typo comments

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 /*
+
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+    il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    va applicato uno sconto del 20% per i minorenni
+    va applicato uno sconto del 40% per gli over 65.
+    L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 */
 
@@ -25,6 +26,7 @@ var cost_trip;
     ==================================
 
     uso parseInt per convertire l'input str in Int
+    e typeof per debuggare il tipo di var
 */
 
 user_age = parseInt(prompt(`Inserisci la tua età:`));
